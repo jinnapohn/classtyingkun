acedfb4ecf32dfdff72acde56413d45e5685e57e
jinnapohn <98334605+jinnapohn@users.noreply.github.com>
2022-02-02 14:28:04 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,9 +1,21 @@
 <html>
-  <head>
-    <title>งานที่ 1</title>
+  <head> 
+    <title>page 1</title> 
   </head>
-  <body>
-    ข้อความ1
-    ข้อความ2
-  </body>
-</html>
+  <body bgcolor="lightblue">
+    <h1 style="color:darkblue;">ประวัติส่วนตัว</h1>
+    <br> 
+    <b>ชื่อ :</b> <i>นางสาวจิณณาภรณ์ สุวรรณศรี</i>
+    <br> 
+    <b>ชื่อเล่น :</b> <i>ชมพู่</i> 
+    <br>
+    <b>ชั้น :</b> <i>ม.4/12</i>
+    <br> 
+    <b>เลขที่ :</b> <i>27</i>
+    <br>
+    <b>เลขประจำตัวนักเรียน :</b> <i>57171</i>
+    <br>
+    <b>โรงเรียน :</b> <i>หล่มสักวิทยาคม<i> 
+    </body>
+    </html>
+เขียนถึง Wanvisa Nittayapan
