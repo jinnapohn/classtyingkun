7b36a15f76282a40c3a68d57e780aeba4383bfbc
jinnapohn <98334605+jinnapohn@users.noreply.github.com>
2022-03-03 10:29:18 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,45 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/Facebook</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td></td>
+        <td><img src="" width="45px"></td>
+        <td><a href="">Twitter</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td></td>
+        <td><img src="" width="45px"></td>
+        <td><a href="">Twitter</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td></td>
+        <td><img src="" width="45px"></td>
+        <td><a href="">Twitter</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td></td>
+        <td><img src="" width="45px"></td>
+        <td><a href="">Twitter</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td></td>
+        <td><img src="" width="45px"></td>
+        <td><a href="">Twitter</a></td>
+      </tr>
+     </table>
+  </body>
+</html>
